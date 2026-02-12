166f6c7f7ac2d639f40ad549bb25901fb03337e3
Pivot <sulukeq1204@gmail.com>
2026-02-12 21:20:27 +0700

    format code
    - sign in fb

--- server/routes/all.routes.js
+++ server/routes/all.routes.js
@@ -4,6 +4,10 @@ const router = express.Router()
 const {register,login,sendOtp, verifyOtp, checkEmailExists, resetPassword} = require('../controllers/users.controller')
 const { loginWithGoogle, googleCallback } = require('../controllers/login_methods/google.controller')
 const passport = require('passport');
+const { ConfigPassport } = require("../controllers/passport.controller");
+const jwt = require("jsonwebtoken");
+ConfigPassport();
+
 
 router.post('/register',register)
 router.post('/login',login)
@@ -17,4 +21,35 @@ router.get("/auth/google/callback", googleCallback);
 
 
 router.get('/login/federated/facebook', passport.authenticate('facebook'));
+
+router.get(
+  "/auth/facebook/callback",
+  passport.authenticate("facebook", {
+    session: false,
+    failureRedirect:
+      "http://localhost:3000/login?error=Facebook%20login%20failed",
+  }),
+  (req, res) => {
+    if (!req.user) {
+      return res.redirect(
+        "http://localhost:3000/login?error=Không%20thể%20lấy%20email%20từ%20facebook",
+      );
+    }
+
+    const token = jwt.sign(
+      {
+        id: req.user._id,
+        fullName: req.user.fullName,
+        email: req.user.email,
+        role: req.user.role,
+        avatarUrl: req.user.avatarUrl || null,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" },
+    );
+
+    res.redirect(`http://localhost:3000/?token=${token}`);
+  },
+);
+
 module.exports =router;
